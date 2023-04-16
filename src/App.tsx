40c4ec3acc9a3f3b02ce6499f1d98e208d19eb8a
Hoàng An <20521051@gm.uit.edu.vn>
2023-04-16 21:54:25 +0700

    modified: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { Footer, Header, SideBar } from "./components/UI";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ErrorLayout } from "./layouts";
+import { Login, Register } from "./pages";
 
-import { Footer, Header } from './components/UI';
-
-function App() {
-
+const ErrorPage = () => {
   return (
-    <div className="App">
+    <div className="Error">
+      <p className="">Oops... Page not found!</p>
+      <ErrorLayout />
+    </div>
+  );
+};
+
+const LoginPage = () => {
+  return (
+    <div className="SignIn">
+      <Login />
+    </div>
+  );
+};
+const RegisterPage = () => {
+  return (
+    <div className="SignUp">
+      <Register />
+    </div>
+  );
+};
+const Home = () => {
+  return (
+    <div className="Home">
       <Header></Header>
+      <div>
+        <SideBar></SideBar>
+      </div>
       <Footer></Footer>
     </div>
-  )
-}
-
-export default App
+  );
+};
+const App = () => (
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/login" Component={LoginPage} />
+        <Route path="/register" Component={RegisterPage} />
+        <Route path="/*" Component={ErrorPage} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+export default App;
