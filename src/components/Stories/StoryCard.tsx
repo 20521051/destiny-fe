d4891abb1e7f6e63b2e9de2e2ccb925397525532
Hoàng An <20521051@gm.uit.edu.vn>
2023-05-07 20:08:28 +0700

    modified: Stories StoryCard

--- src/components/Stories/StoryCard.tsx
+++ src/components/Stories/StoryCard.tsx
@@ -17,13 +17,15 @@ const StoryCard = ({ story, userImage }: StoryCardProps) => {
     }
   }
   return (
-    <div>
-      <video muted ref={videoRef} width={80} height={100} className=''>
-        <source src={story} type='video/mp4' />
-      </video>
-      <div className='w-[40px] h-[40px] rounded-full overflow-hidden flex' aria-hidden='true'>
+    <div className='min-w-[160px] w-[160px] h-[220px] mx-[3px] rounded-xl border-slate-300 border-2 relative overflow-hidden flex'>
+      <div className='w-[40px] h-[40px] rounded-full overflow-hidden flex absolute top-3 left-4' aria-hidden='true'>
         <img src={userImage} alt='avatar' className='inline-block object-cover' />
       </div>
+      <img className='inline-block object-fill' src='/src/assets/images/CallofDuty.jpg' alt='img' />
+      {/* <video muted ref={videoRef} width={80} height={100} className=''>
+        <source src={story} type='video/mp4' />
+      </video> */}
+      <h3 className='absolute bottom-6 left-8 text-white text-2xl font-bold'>Jeff</h3>
     </div>
   )
 }
