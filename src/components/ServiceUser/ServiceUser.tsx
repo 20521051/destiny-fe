c6ab62451373d6c9ada537f189208a69a4bb550a
minhtu752002 <20520836@gm.uit.edu.vn>
2023-05-08 15:21:38 +0700

    feat: Component Service User phục vụ người dùng được những tựa game nào

---
+++ src/components/ServiceUser/ServiceUser.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ServiceUser = () => {
+  return (
+    <div className='rounded-2xl	 border-2 border-slate-300 w-[770px] h-[130px]'>
+      <div className='w-[200px] h-[50px] flex overflow-hidden rounded-2xl relative bg-black' aria-hidden='true'>
+        <img src='/src/assets/images/lienminhhuyenthoai.jpg' alt='avatar' className='w-full inline-block object-fill brightness-50' />
+        <p className='absolute font-bold text-white top-[50%] left-[50%] translate-x-[-40%] translate-y-[-50%] w-full'>
+          Liên Minh Huyền Thoại
+        </p>
+      </div>
+    </div>
+  )
+}
+export default ServiceUser
